a6c7fb3b0a7b47b10def641894d265b8265cc154
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-10 14:39:54 +0530

    Update AbilityManager.java

--- src/main/java/com/mythicabilities/abilities/AbilityManager.java
+++ src/main/java/com/mythicabilities/abilities/AbilityManager.java
@@ -1,7 +1,6 @@
 package com.mythicabilities.abilities;
 
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -44,27 +43,4 @@ public class AbilityManager {
     public Map<String, Ability> getAllAbilities() {
         return abilities;
     }
-}
-
-abstract class Ability {
-    protected String name;
-    protected String displayName;
-    protected int cooldown;
-    protected ItemStack icon;
-    
-    public Ability(String name, String displayName, int cooldown, ItemStack icon) {
-        this.name = name;
-        this.displayName = displayName;
-        this.cooldown = cooldown;
-        this.icon = icon;
-    }
-    
-    public abstract void onRightClick(Player player);
-    public abstract void onLeftClick(Player player);
-    public abstract void onCombo(Player player, Player target);
-    
-    public String getName() { return name; }
-    public String getDisplayName() { return displayName; }
-    public int getCooldown() { return cooldown; }
-    public ItemStack getIcon() { return icon.clone(); }
 }
