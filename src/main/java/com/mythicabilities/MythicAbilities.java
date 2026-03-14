066fa565959f7dac38ab3c327414284ee59082a8
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-14 15:38:56 +0530

    Update MythicAbilities.java

--- src/main/java/com/mythicabilities/MythicAbilities.java
+++ src/main/java/com/mythicabilities/MythicAbilities.java
@@ -1,11 +1,9 @@
 package com.mythicabilities;
 
-import com.mythicabilities.abilities.AbilityManager;
-import com.mythicabilities.abilities.InfernoTouch;
-import com.mythicabilities.abilities.FrostWalker;
-import com.mythicabilities.admin.GiveAbilityCommand;
+import com.mythicabilities.abilities.*;
 import com.mythicabilities.admin.AdminCommand;
 import com.mythicabilities.admin.AdminPanelGUI;
+import com.mythicabilities.admin.GiveAbilityCommand;
 import com.mythicabilities.admin.SMPManager;
 import com.mythicabilities.admin.WorldBorderManager;
 import com.mythicabilities.listeners.PlayerJoinListener;
@@ -13,6 +11,7 @@ import com.mythicabilities.listeners.AbilityUseListener;
 import com.mythicabilities.listeners.WorldBorderListener;
 import com.mythicabilities.gui.AbilitySpinGUI;
 import com.mythicabilities.utils.CooldownManager;
+import com.mythicabilities.utils.TitleBar;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MythicAbilities extends JavaPlugin {
@@ -23,6 +22,7 @@ public class MythicAbilities extends JavaPlugin {
     private AdminCommand adminCommand;
     private SMPManager smpManager;
     private WorldBorderManager worldBorderManager;
+    private AbilitySpinGUI spinGUI;
     
     @Override
     public void onEnable() {
@@ -32,11 +32,12 @@ public class MythicAbilities extends JavaPlugin {
         saveDefaultConfig();
         
         // Initialize managers
-        this.cooldownManager = new CooldownManager();
+        this.cooldownManager = new CooldownManager(this); // Pass plugin instance for cooldown display
         this.abilityManager = new AbilityManager();
         this.smpManager = new SMPManager(this);
         this.worldBorderManager = new WorldBorderManager(this);
         this.adminCommand = new AdminCommand(this);
+        this.spinGUI = new AbilitySpinGUI(this);
         
         // Register abilities
         registerAbilities();
@@ -46,25 +47,42 @@ public class MythicAbilities extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new AbilityUseListener(this), this);
         getServer().getPluginManager().registerEvents(new WorldBorderListener(this), this);
         getServer().getPluginManager().registerEvents(new AdminPanelGUI(this, adminCommand), this);
+        getServer().getPluginManager().registerEvents(spinGUI, this); // Register spin GUI listener
         
         // Register commands
-        getCommand("abilities").setExecutor(new AbilitySpinGUI(this));
+        getCommand("abilities").setExecutor(spinGUI);
         getCommand("smp").setExecutor(adminCommand);
         getCommand("admin").setExecutor(adminCommand);
         getCommand("giveability").setExecutor(new GiveAbilityCommand(this));
         
         getLogger().info("MythicAbilities has been enabled successfully!");
         getLogger().info("Supporting Minecraft 1.21.11 with latest features!");
+        getLogger().info("Loaded " + abilityManager.getAllAbilities().size() + " abilities!");
     }
     
     private void registerAbilities() {
+        // Register all abilities
         abilityManager.registerAbility(new InfernoTouch(this));
         abilityManager.registerAbility(new FrostWalker(this));
-        // More abilities will be added here
+        abilityManager.registerAbility(new VoidWalker(this));
+        abilityManager.registerAbility(new MandelaEffect(this));
+        abilityManager.registerAbility(new NaturesWrath(this));
+        abilityManager.registerAbility(new AerialMace(this));
+        abilityManager.registerAbility(new KagurasUmbrella(this));
+        abilityManager.registerAbility(new SuyousTranscendence(this));
+        abilityManager.registerAbility(new LingsShadow(this));
+        abilityManager.registerAbility(new FirstOfStone(this));
+        
+        getLogger().info("Registered 10 abilities successfully!");
     }
     
     @Override
     public void onDisable() {
+        // Clean up any active abilities
+        if (abilityManager != null) {
+            // Additional cleanup if needed
+        }
+        
         getLogger().info("MythicAbilities has been disabled!");
     }
     
@@ -91,4 +109,8 @@ public class MythicAbilities extends JavaPlugin {
     public WorldBorderManager getWorldBorderManager() {
         return worldBorderManager;
     }
+    
+    public AbilitySpinGUI getSpinGUI() {
+        return spinGUI;
     }
+}
